9875f81ea855999c774418400f8ccbcf747aaddb
tamara-taylor <tamaralynntaylor@yahoo.com>
2022-01-15 13:35:26 -0500

    working on it

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,7 +1,10 @@
 const router = require("express").Router();
 const { checkUsernameExists, validateRoleName } = require('./auth-middleware');
 const { JWT_SECRET } = require("../secrets"); // use this secret!
+const User = require('../users/users-model')
+const bcrypt = require('bcryptjs')
 
+const jwt = require('jsonwebtoken')
 router.post("/register", validateRoleName, (req, res, next) => {
   /**
     [POST] /api/auth/register { "username": "anna", "password": "1234", "role_name": "angel" }
@@ -14,8 +17,17 @@ router.post("/register", validateRoleName, (req, res, next) => {
       "role_name": "angel"
     }
    */
+  let user = req.body
+  const hash = bcrypt.hashSync(req.body.password, 8)
+  user.password = hash
+  User.add(user)
+  .then(newUser => {
+    res.status(201).json(newUser)
+  })
+  .catch(err => {
+    res.status(201).json({message: err.message})
+  })
 });
-
 
 router.post("/login", checkUsernameExists, (req, res, next) => {
   /**
@@ -37,6 +49,31 @@ router.post("/login", checkUsernameExists, (req, res, next) => {
       "role_name": "admin" // the role of the authenticated user
     }
    */
+  let {username, password} = req.body
+  User.findBy({username})
+  .then(([user]) => {
+    if(user && bcrypt.compareSync(password, user.password)){
+      const token = makeToken(user)
+      res.status(200).json({message: `${user.username} is back!`, token})
+    }else{
+      res.status(401).json({message: 'Invalid credentials'})
+    }
+  })
+  .catch(err => {
+    res.status(500).json({message: err.message})
+  })
 });
+
+function makeToken(user){
+  const payload = {
+    subject: user.user_id,
+    username: user.username,
+    role_name: user.role_name
+  }
+  const options = {
+    expiresIn: '1d'
+  }
+  return jwt.sign(payload, jwtSecret, options)
+}
 
 module.exports = router;
